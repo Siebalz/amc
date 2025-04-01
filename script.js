c51d3d714c21c5b54b511be7cdc6f1315c06cde5
Siebalz <balztea39@gmail.com>
2025-04-01 17:16:24 +0700

    second commit

--- script.js
+++ script.js
@@ -53,3 +53,24 @@ function register(){
     })
     .catch(error => console.error("Error:", error));
 }
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text")
+
+      modeSwitch.addEventListener("click", () =>{
+        body.classList.toggle("dark");
+        if (body.classList.contains("dark")){
+            modeText.innerText = "Light Mode"
+          }else{
+            modeText.innerText = "Dark Mode"
+          }
+      })
+
+      toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+      })
+
+
+        
